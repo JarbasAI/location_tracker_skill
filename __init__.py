4c09efc3d82fa5b39deeebd50e0776234f605583
jarbasai <jarbasai>
2017-08-17 03:44:16 +0100

    update

--- __init__.py
+++ __init__.py
@@ -29,29 +29,141 @@ LOGGER = getLogger(__name__)
 
 # TODO make configurable to get location from other sources (GPS)
 
+
 class LocationTrackerSkill(MycroftSkill):
     def __init__(self):
         super(LocationTrackerSkill, self).__init__()
-        self.minutes = 15
-        self.source = "ip"
+        self.minutes = self.config.get("location").get("update_mins", 15)
+        self.source = self.config.get("location").get("update_source", "ip")
+        self.active = self.config.get("location").get("tracking", True)
+        self.auto_context = self.config.get("location").get("auto_context",True)
+        self.active = True
         self.timer = Timer(60 * self.minutes, self.get_location)
+        self.timer.setDaemon(True)
 
     def initialize(self):
-        self.__build_update_intent()
-
-    def __build_update_intent(self):
         intent = IntentBuilder("UpdateLocationIntent") \
-            .require("UpdateKeyword").build()
+            .require("UpdateKeyword").require(
+            "LocationKeyword").optionally("ConfigKeyword").build()
         self.register_intent(intent, self.handle_update_intent)
+
+        intent = IntentBuilder("CurrentLocationIntent") \
+            .require("CurrentKeyword").require("LocationKeyword").build()
+        self.register_intent(intent, self.handle_current_location_intent)
+
+        intent = IntentBuilder("UnSetLocationTrackingIntent") \
+            .require("TrackingKeyword").require("LocationKeyword").require(
+            "DeactivateKeyword").build()
+        self.register_intent(intent, self.handle_deactivate_tracking_intent)
+
+        intent = IntentBuilder("SetLocationTrackingIntent") \
+            .require("TrackingKeyword").require("LocationKeyword").require(
+            "ActivateKeyword").build()
+        self.register_intent(intent, self.handle_activate_tracking_intent)
+
+        intent = IntentBuilder("WhereAmIIntent") \
+            .require("WhereAmIKeyword").build()
+        self.register_intent(intent, self.handle_where_am_i_intent)
+
+        intent = IntentBuilder("SetLocationContextIntent") \
+            .require("InjectionKeyword").require(
+            "LocationKeyword").require(
+            "ActivateKeyword").build()
+        self.register_intent(intent, self.handle_activate_context_intent)
+
+        intent = IntentBuilder("UnsetLocationContextIntent") \
+            .require("InjectionKeyword").require(
+            "LocationKeyword").require(
+            "DeactivateKeyword").build()
+        self.register_intent(intent, self.handle_deactivate_context_intent)
+
+        if self.active:
+            self.timer.start()
+
+    def handle_deactivate_context_intent(self, message):
+        if not self.auto_context:
+            self.speak("Location context injection is not active")
+        else:
+            self.auto_context = False
+            self.speak("Location context injection deactivated")
+
+    def handle_activate_context_intent(self, message):
+        if self.auto_context:
+            self.speak("Location context injection is already active")
+        else:
+            self.auto_context = True
+            self.speak("Location context injection activated")
+
+    def handle_deactivate_tracking_intent(self, message):
+        if not self.active:
+            self.speak("Location tracking from " + self.source + " is not active")
+        else:
+            self.active = False
+            self.timer.cancel()
+            self.speak("Location tracking from " + self.source + " deactivated")
+
+    def handle_activate_tracking_intent(self, message):
+        if self.active:
+            self.speak("Location tracking from " + self.source + " is active")
+        else:
+            self.active = True
+            self.timer.start()
+            self.speak("Location tracking from " + self.source + " activated")
+
+    def handle_current_location_intent(self, message):
+        config = self.config_core.get("location")
+        city = config.get("city", {}).get("name", "unknown city")
+        country = config.get("city", {}).get("region").get("country").get(
+            "name", "unknown country")
+        self.speak("configuration location is " + city + ", " +
+                   country)
+        if self.auto_context:
+            self.set_context('Location', city + ', ' + country)
+
+    def handle_where_am_i_intent(self, message):
+        ip = message.context.get("ip")
+        destinatary = message.context.get("destinatary")
+        if "fbchat" in destinatary:
+            # TODO check profile page
+            self.speak("I don't know you location and i won't check your "
+                       "profile for it")
+            return
+        if ip:
+            config = self.from_ip(update=False)
+            if config != {}:
+                city = config.get("location", {}).get("city", {}).get("name","unknown city")
+                country = config.get("location", {}).get("city", {}).get(
+                    "region").get("country").get("name", "unknown country")
+                self.speak(
+                    "your ip adress says you are in " + city + " in " +
+                    country)
+        elif ":" in destinatary:
+            sock = destinatary.split(":")[0]
+            # TODO user from sock
+            self.speak("ask me later")
+            return
+        else:
+            config = self.get_location()
+            if config != {}:
+                city = config.get("location", {}).get("city", {}).get("name",
+                                                                      "unknown city")
+                country = config.get("location", {}).get("city", {}).get(
+                    "region").get("country").get("name", "unknown country")
+                self.speak(
+                    "your ip adress says you are in " + city + " in " +
+                    country)
 
     def handle_update_intent(self, message):
         if connected():
             self.speak("updating location from ip address")
-            self.get_location("ip")
+            config = self.get_location("ip")
+            city = config.get("city", {}).get("name", "unknown city")
+            country = config.get("city", {}).get("region").get("country").get(
+                "name", "unknow country")
         else:
             self.speak("Cant do that offline")
 
-    def from_ip(self):
+    def from_ip(self, update = True):
         self.log.info("Retrieving location data from ip adress")
         if connected():
             response = unirest.get("https://ipapi.co/json/")
@@ -75,23 +187,34 @@ class LocationTrackerSkill(MycroftSkill):
             location_data = {"city": city_data, "coordinate": coordinate_data,
                              "timezone": timezone_data}
             config = {"location": location_data}
-            self.emitter.emit(Message("configuration.patch", {"config": config}))
+            if update:
+                try:
+                    # jarbas core skill function
+                    self.config_update(config)
+                except:
+                    pass
+            return config
         else:
             self.log.warning("No internet connection, could not update "
                              "location from ip adress")
+            return {}
 
     def get_location(self, source=None):
         if source is None:
             source = self.source
         if source == "ip":
-            self.from_ip()
+            config = self.from_ip()
+            if config != {}:
+                city = config.get("location", {}).get("city", {}).get("name",
+                                                                      "unknown city")
+                country = config.get("location", {}).get("city", {}).get(
+                    "region").get("country").get("name", "unknown country")
+                if self.auto_context:
+                    self.set_context('Location', city + ', ' + country)
         else:
             self.log.info("Failed to retrieve location data from " + source)
-        return
-
-    def stop(self):
-        self.timer.cancel()
-        self.timer = Timer(60 * self.minutes, self.get_location)
+            config = {}
+        return config
 
 
 def create_skill():
